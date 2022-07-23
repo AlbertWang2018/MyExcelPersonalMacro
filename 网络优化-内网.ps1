d899f460fd6ba399c292d6c29368464de739f927
Albert.Wang <36392874+AlbertWang2018@users.noreply.github.com>
2022-07-23 08:49:38 +0800

    调节metric变更interface优先级达到切换不同网上上网的目的

---
+++ 网络优化-内网.ps1
@@ -0,0 +1,8 @@
+﻿$a=(Get-NetAdapter -InterfaceDescription *pan*).ifIndex 
+#$a
+$b=Get-NetRoute -DestinationPrefix 0.0.0.0/0 -ifindex $a
+#$b
+$c=(Get-NetIPAddress -InterfaceIndex $a).IPAddress
+$d="route change 0.0.0.0 mask 0.0.0.0 $c metric 90 if $a"
+$d
+cmd /c $d
